259be65dc212bbf0b220b3670a970db5da8a21c9
Darlan Mendes <dedsondarlan@gmail.com>
2022-09-20 08:36:51 -0300

    Adição de função para detectar dispositivo utilizado pelo o usuário

--- script/script.js
+++ script/script.js
@@ -9,7 +9,7 @@ let palavraDigitadaComparada;
 let resultadoArea = document.getElementById("resultado");
 let inputInicial = document
 
-
+detectBrowser();
 
 inputInicial.addEventListener("keyup", (e) => {
     let valorInputAtual = document.getElementById(`${linhaSelecionada}` + `${colunaSelecionada}`);
@@ -96,4 +96,28 @@ const ativarProximaLinha = () => {
         letrasP[col].disabled = false;
     })
 
+}
+function detectBrowser() {
+
+    let userAgent = navigator.userAgent;
+    let browserName;
+
+    if (userAgent.match(/chrome|chromium|crios/i)) {
+        browserName = "Chrome";
+    } else if (userAgent.match(/firefox|fxios/i)) {
+        browserName = "Firefox";
+    } else if (userAgent.match(/safari/i)) {
+        browserName = "Safari";
+    } else if (userAgent.match(/opr\//i)) {
+        browserName = "Opera";
+    } else if (userAgent.match(/edg/i)) {
+        browserName = "Edge";
+    } else if (userAgent.match(/android/i)) {
+        browserName = "Android";
+    } else if (userAgent.match(/iphone/i)) {
+        browserName = "iPhone";
+    } else {
+        browserName = "Unknown";
+    }
+    console.log('navegador é:',browserName);
 }
