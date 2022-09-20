9f545cbd1d4382a266f2240a426120044914f1c7
Darlan Mendes <dedsondarlan@gmail.com>
2022-09-20 00:53:52 -0300

    Adição de adEventListener para digitar pelo teclado

--- script/script.js
+++ script/script.js
@@ -1,72 +1,99 @@
 let linhaSelecionada = 1;
-let palavraDoDia = "sorte";
-let letras = [];
+let colunaSelecionada = 1;
+let palavraDoDia = ['s', 'o', 'r', 't', 'e'];
+let letras = ['', '', '', '', ''];
 let letrasA = [];
 let letrasP = [];
 let palavraDigitada;
-let resultadoArea=document.getElementById("resultado");
-const submmit = () => {
-    letras[0] = document.getElementById(`${linhaSelecionada}` + "1").value.toLowerCase();
-    letras[1] = document.getElementById(`${linhaSelecionada}` + "2").value.toLowerCase();
-    letras[2] = document.getElementById(`${linhaSelecionada}` + "3").value.toLowerCase();
-    letras[3] = document.getElementById(`${linhaSelecionada}` + "4").value.toLowerCase();
-    letras[4] = document.getElementById(`${linhaSelecionada}` + "5").value.toLowerCase();
+let palavraDigitadaComparada;
+let resultadoArea = document.getElementById("resultado");
+let inputInicial = document
 
 
-    for (i = 0; i < letras.length; i++) {
-        document.getElementById(`${linhaSelecionada}` + `${i + 1}`).style.backgroundColor = "red"
-        document.getElementById(`${linhaSelecionada}` + `${i + 1}`).style.color = "white"
-        for (j = 0; j < palavraDoDia.length; j++) {
 
-            if (letras[i] === palavraDoDia[j]) {
-                document.getElementById(`${linhaSelecionada}` + `${i + 1}`).style.backgroundColor = "yellow"
-                document.getElementById(`${linhaSelecionada}` + `${i + 1}`).style.color = "black"
+inputInicial.addEventListener("keyup", (e) => {
+    let valorInputAtual = document.getElementById(`${linhaSelecionada}` + `${colunaSelecionada}`);
+
+    if (e.key === 'Enter') {
+        submmit();
+
+    } else {
+        if (e.key === 'Backspace') {
+
+            valorInputAtual.value = '';
+            colunaSelecionada > 1 && colunaSelecionada--;
+
+        } else {
+            if (valorInputAtual.value === '' && colunaSelecionada <= 5) {
+
+                valorInputAtual.value = e.key
+            }
+            if (colunaSelecionada < 5) {
+                colunaSelecionada++
             }
 
-            if (letras[i] === palavraDoDia[i]) {
-                document.getElementById(`${linhaSelecionada}` + `${i + 1}`).style.backgroundColor = "green"
-                document.getElementById(`${linhaSelecionada}` + `${i + 1}`).style.color = "white"
-            }
-
-
+            //console.log(e)
         }
-        palavraDigitada = letras.join("");
-
 
     }
-    if (palavraDigitada === palavraDoDia) {
-        
-        resultadoArea.innerHTML = "<div>Você acertou!! A palavra do dia é :" + `${palavraDoDia.toUpperCase()}` + "<h1>";
-        resultadoArea.style.backgroundColor="green";
-        resultadoArea.style.color="white"
-    } else {
-        if (linhaSelecionada < 5) {
-            ++linhaSelecionada
-            resultadoArea.innerHTML = "<div> Você tem mais " + `${6 - linhaSelecionada}` + " tentativas<div>";
-            ativarProximaLinha()
-        } else {
-            resultadoArea.innerHTML = "<div>Boa tentativa<div>";
-        }
 
+
+})
+
+const submmit = () => {
+    if (linhaSelecionada < 6) {
+        letras.map((item, col) => {
+            letras[col] = document.getElementById(`${linhaSelecionada}` + `${col + 1}`).value.toLowerCase();
+            document.getElementById(`${linhaSelecionada}` + `${col + 1}`).style.backgroundColor = "red";
+            document.getElementById(`${linhaSelecionada}` + `${col + 1}`).style.color = "white";
+            palavraDoDia.map((letra) => {
+
+                if (letras[col] === letra) {
+                    document.getElementById(`${linhaSelecionada}` + `${col + 1}`).style.backgroundColor = "yellow";
+                    document.getElementById(`${linhaSelecionada}` + `${col + 1}`).style.color = "black";
+                }
+            })
+            palavraDigitada = letras.join("")
+            if (letras[col] === palavraDoDia[col]) {
+                document.getElementById(`${linhaSelecionada}` + `${col + 1}`).style.backgroundColor = "green";
+                document.getElementById(`${linhaSelecionada}` + `${col + 1}`).style.color = "white";
+            }
+
+        })
+        palavraDoDiaComparada = palavraDoDia.join("");
+        console.log(palavraDoDiaComparada, palavraDigitada)
+        if (palavraDigitada === palavraDoDiaComparada) {
+
+            resultadoArea.innerHTML = "<div>Você acertou!! A palavra do dia é :" + `${palavraDoDiaComparada.toUpperCase()}` + "<h1>";
+            resultadoArea.style.backgroundColor = "green";
+            resultadoArea.style.color = "white"
+        } else {
+            if (linhaSelecionada < 5) {
+                ++linhaSelecionada;
+                colunaSelecionada = 1;
+                if (6 - linhaSelecionada > 1) {
+                    resultadoArea.innerHTML = "<div> Você tem mais " + `${6 - linhaSelecionada}` + " tentativas<div>"
+                } else {
+                    resultadoArea.innerHTML = "<div> Você tem mais 1 tentativa<div>"
+                }
+
+                ativarProximaLinha();
+            } else {
+                resultadoArea.innerHTML = "<div>Boa tentativa<div>";
+            }
+
+        }
     }
 }
 const ativarProximaLinha = () => {
-    //seleciona linha submetida
-    letrasA[1] = document.getElementById(`${linhaSelecionada - 1}` + "1")
-    letrasA[2] = document.getElementById(`${linhaSelecionada - 1}` + "2")
-    letrasA[3] = document.getElementById(`${linhaSelecionada - 1}` + "3")
-    letrasA[4] = document.getElementById(`${linhaSelecionada - 1}` + "4")
-    letrasA[5] = document.getElementById(`${linhaSelecionada - 1}` + "5")
-    //seleciona proxima linha
-    letrasP[1] = document.getElementById(`${linhaSelecionada}` + "1")
-    letrasP[2] = document.getElementById(`${linhaSelecionada}` + "2")
-    letrasP[3] = document.getElementById(`${linhaSelecionada}` + "3")
-    letrasP[4] = document.getElementById(`${linhaSelecionada}` + "4")
-    letrasP[5] = document.getElementById(`${linhaSelecionada}` + "5")
-
-    for (let i = 1; i <= 5; i++) {
+    letras.map((item, col) => {
+        //seleciona linha submetida
+        letrasA[col] = document.getElementById(`${linhaSelecionada - 1}` + `${col + 1}`);
+        //seleciona proxima linha
+        letrasP[col] = document.getElementById(`${linhaSelecionada}` + `${col + 1}`);
         //desabilita linhas submetidas e ativa proxima  
-        letrasA[i].disabled = true;
-        letrasP[i].disabled = false;
-    }
+        letrasA[col].disabled = true;
+        letrasP[col].disabled = false;
+    })
+
 }
